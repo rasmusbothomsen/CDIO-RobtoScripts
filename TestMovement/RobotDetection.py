6932e5d5855f95f80a7e3a6b85061b959d2b4625
rasmusbothomsen <rasmus0408@gmail.com>
2023-06-14 09:40:02 +0200

    Implementation of robot detection

--- TestMovement/RobotDetection.py
+++ TestMovement/RobotDetection.py
@@ -49,23 +49,36 @@ def detectRobot(image_path):
     edges = cv2.Canny(blurred, 50, 150)
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     triangle_info = {}
-
+    triangle_contour = []
     for cont in contours:
         perimeter = cv2.arcLength(cont, True)
         approx = cv2.approxPolyDP(cont, 0.04 * perimeter, True)
         area = cv2.contourArea(cont)
         if len(approx) == 3 and 700 < area < 2000:
-            cv2.drawContours(image, [approx], 0, (0, 0, 255), 2)
-            tip_point, base_points = FrontAndBack(approx[:, 0])
-            cv2.circle(image, tuple(tip_point), 5, (255, 0, 0), -1)
-            cv2.putText(image, 'Front', (tip_point[0], tip_point[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
-            #Finder midtpunktet af grundlinjen for trekanten
-            mid_base_point = np.mean(base_points, axis=0).astype(int)
-            cv2.circle(image, tuple(mid_base_point), 5, (0, 255, 0), -1)
-            cv2.putText(image, 'Back', (mid_base_point[0], mid_base_point[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-            triangle_info['front'] = tuple(tip_point)
-            triangle_info['back'] = tuple(mid_base_point)
+            triangle_contour.append(approx)
             break
+
+    #Calculate normalized contour coordinates
+    triangle_contour = np.squeeze(triangle_contour)
+    triangle_contour = triangle_contour / (image.shape[1], image.shape[0])
+
+    image = scaleImage(image,80)
+    #revert back to pixel coordinates with scaled images
+    triangle_contour = triangle_contour * (image.shape[1], image.shape[0])
+    triangle_contour = np.expand_dims(triangle_contour, axis=1)
+    approx = triangle_contour.astype(np.int32)
+
+
+    cv2.drawContours(image, [approx], 0, (0, 0, 255), 2)
+    tip_point, base_points = FrontAndBack(approx[:, 0])
+    cv2.circle(image, tuple(tip_point), 5, (255, 0, 0), -1)
+    cv2.putText(image, 'Front', (tip_point[0], tip_point[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+    #Finder midtpunktet af grundlinjen for trekanten
+    mid_base_point = np.mean(base_points, axis=0).astype(int)
+    cv2.circle(image, tuple(mid_base_point), 5, (0, 255, 0), -1)
+    cv2.putText(image, 'Back', (mid_base_point[0], mid_base_point[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    triangle_info['front'] = tuple(tip_point)
+    triangle_info['back'] = tuple(mid_base_point)
 
     cv2.imshow("Shapes", image)
     cv2.waitKey(0)
@@ -73,7 +86,7 @@ def detectRobot(image_path):
 
     return triangle_info
 
-image_path = "/Users/berfinfloraturan/Downloads/robot/en/RobotBillede1.jpg"
+image_path = r"C:\Users\rasmu\Downloads\351638293_785027243207998_7844219772980120963_n.jpg"
 triangle_info = detectRobot(image_path)
 print(triangle_info.get("front"))
 print(triangle_info.get("back"))
